c24eaa942ec0649a19f1194457865d756e492dec
GameznaluNL <gameznalu02@gmail.com>
2025-10-20 08:29:34 -0300

    Proyecto Completo
    Proyecto Completo

---
+++ ST/src/test/java/SuperTecnologia/ST/StApplicationTests.java
@@ -0,0 +1,13 @@
+package SuperTecnologia.ST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
